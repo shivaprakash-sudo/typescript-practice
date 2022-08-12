80fcdb54aa962117ed0937c2fd2363fb28daad87
Shiva Prakash Pendem <pendemsivaprakash@gmail.com>
2022-08-12 01:56:47 +0100

    basic types

---
+++ script.js
@@ -0,0 +1,6 @@
+var myName = "Shiva";
+var age = 23;
+function printNameAndAge(name, age) {
+    console.log("".concat(name, " is ").concat(age, " years old."));
+}
+printNameAndAge(myName, age);
---
+++ script.ts
@@ -0,0 +1,9 @@
+let myName = "Shiva";
+
+let age = 23;
+
+function printNameAndAge(name: string, age: number) {
+  console.log(`${name} is ${age} years old.`);
+}
+
+printNameAndAge(myName, age);
