464a45c93e0b531f5c3158420b35469712451ec6
Shiva Prakash Pendem <pendemsivaprakash@gmail.com>
2022-08-12 18:50:34 +0100

    function signatures

--- src/script.ts
+++ src/script.ts
@@ -17,6 +17,28 @@ const doSomeMoreMagic = (user: objWithName) => {
 }
 
 doMagic(1234, "Banana")
-const userObj = {name: "Shiva", uid:"sdgnklsd"};
-doSomeMagic(userObj)
-doSomeMoreMagic(userObj)
+const userObj = { name: "Shiva", uid: "sdgnklsd" };
+doSomeMagic(userObj);
+doSomeMoreMagic(userObj);
+
+
+// function signature
+let calc: (a: number, b: number, operation?: string) => string | number
+
+calc = (a, b, operation) => {
+  if (operation === "add" || operation === "addition") {
+    return a + b;
+  } 
+  if (operation === "subtract" || operation === "subtraction") {
+    return a - b;
+  }
+  if (operation === "multiply" || operation === "multiplication") {
+    return a * b;
+  } 
+  if (operation === "divide" || operation === "division") {
+    return a / b;
+  }
+  return "Please provide an operation to perform the calculation";
+}
+
+console.log(calc(3, 0, "divide"));
